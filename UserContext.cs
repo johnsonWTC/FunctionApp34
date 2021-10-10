f9e7da8c2497e86f4247971e972f79d345af2117
Johnson Dubula <jdubula@iqbusiness.net>
2021-10-10 21:59:35 +0200

    Functions

---
+++ UserContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionApp34
+{
+    class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
